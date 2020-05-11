f2342565b0d059040de1a696a46f646e7a0ecf5b
Abhi <64596494+abhiprojectz@users.noreply.github.com>
2020-05-11 22:53:45 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,31 @@
+//Edit as per your preference...
+
+
+
+function config() {
+
+    
+
+    var username = "abhiprojectz"; //Replace with your username 
+
+    
+
+    var blogname = "Github Portfolio CMS";
+
+    
+
+    //Replace with your blogName
+
+    
+
+    var about = "Github Portfolio CMS is a static portfolio geneartor to make a showcase of your projects!";
+
+    
+
+    //replace with your blog description!
+
+    
+
+    return username,blogname,about;
+
+}
