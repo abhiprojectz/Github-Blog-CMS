e4ce4db25f2d2cb4b846dcea373e0f5759c5e327
Abhi <64596494+abhiprojectz@users.noreply.github.com>
2020-05-12 20:05:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,33 @@
 //Edit as per your preference...
 
+function initialize(database) {
+
+    
+
+    var database = {
+
+        "name": "Abhiprojectz",
+
+        "repo": "Github-Blog-CMS",
+
+        "folder": "posts",
+
+        "pages": "pages",
+
+        "blogname": "Github Portfolio CMS",
+
+        "headline": "My blog on Github!",
+
+        "about": "Github blog CMS is a static blog geneartor to make a showcase of your projects! This is a Github Blog CMS! \n \n By using this you can create your own blog for free! \n \n Easily too it supports markdown!\n \n Created By: Abhi for Github Community!"
+
+        };
+
+        
+
+        return database;
+
+        
+
+        }
+
 
